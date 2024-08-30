11e8722811c723670ea1a6f5efd76aac93c25601
marvin4espinoz <marvinespinoza.jr.gmail.com>
2024-08-30 16:21:11 -0400

    updating models, dbt_project with mart and intermediate, and updated dbt_project.yml file

---
+++ gsnc_dbt_project/models/staging/stg__enrollment_schools_race_gender.sql
@@ -0,0 +1,63 @@
+-- staging model that stacks, with specific columns, traditional and charter school enrollment by race and gender
+
+-- staging model that stacks, with specific columns, traditional and charter school enrollment by race and gender
+
+with traditional as (
+
+    select
+        LEA,
+        School,
+        ____LEA_Name____ as district_name,
+        ___School_Name___ as school_name,
+        Indian_Male,
+        Indian_Female,
+        Asian_Male,
+        Asian_Female,
+        Hispanic_Male,
+        Hispanic_Female,
+        Black_Male,
+        Black_Female,
+        White_Male,
+        White_Female,
+        Pacific_Island_Female,
+        Pacific_Island_Male,
+        Two_or__More_Male,
+        Two_or__More_Female,
+        Total,
+        reporting_year,
+        school_type
+    from {{ ref('stg__enrollment_traditional_race_gender') }}
+
+),
+
+charter as (
+
+    select
+        CS as LEA,
+        '000' as School,
+        'Charter LEA' as district_name,
+        Charter_School_Name as school_name,
+        INDIANMale as Indian_Male,
+        INDIANFemale as Indian_Female,
+        ASIANMale as Asian_Male,
+        ASIANFemale as Asian_Female,
+        HISPANICMale as Hispanic_Male,
+        HISPANICFemale as Hispanic_Female,
+        BLACKMale as Black_Male,
+        BLACKFemale as Black_Female,
+        WHITEMale as White_Male,
+        WHITEFemale as White_Female,
+        PACIFICISLANDMale as Pacific_Island_Male,
+        PACIFICISLANDFemale as Pacific_Island_Female,
+        TWO_OR_MORE_RACESMale as Two_or__More_Male,
+        TWO_OR_MORE_RACESFemale as Two_or__More_Female,
+        Total,
+        reporting_year,
+        school_type
+    from {{ ref('stg__enrollment_charter_race_gender') }}
+
+)
+
+select * from traditional
+UNION ALL
+select * from charter
---
+++ gsnc_dbt_project/models/staging/stg__enrollment_traditional_race_gender.sql
@@ -0,0 +1,38 @@
+-- this creates staging data model / table that stacks enrollment by race and gender for all schools
+
+
+{% set schema = target.dataset %}
+
+{{ log("Debug: schema = " ~ schema, info=True) }}
+
+{% set table_list_query %}
+  SELECT table_name
+  FROM `{{ target.project }}.{{ schema }}.INFORMATION_SCHEMA.TABLES`
+  WHERE table_name LIKE 'raw_nc_schools_enrollment_traditional_race_gender_sy_%'
+{% endset %}
+
+{{ log("Debug: table_list query = " ~ table_list_query, info=True) }}
+
+{% set results = run_query(table_list_query) %}
+
+{% if execute %}
+  {% set tables = results.columns[0].values() %}
+  {{ log("Debug: tables found = " ~ tables | join(", "), info=True) }}
+{% else %}
+  {% set tables = [] %}
+{% endif %}
+
+WITH raw_source AS (
+  {% for table in tables %}
+    SELECT
+      {% for column in adapter.get_columns_in_relation(source('nc_school_database', table)) %}
+        CAST({{ column.name }} AS STRING) AS {{ column.name }}{% if not loop.last %},{% endif %}
+      {% endfor %},
+      CAST(SUBSTR('{{ table }}', -5, 2) || '-' || SUBSTR('{{ table }}', -2, 2) AS STRING) AS reporting_year,
+	  'Traditional District' as school_type
+    FROM {{ source('nc_school_database', table) }}
+    {% if not loop.last %}UNION ALL{% endif %}
+  {% endfor %}
+)
+
+SELECT * FROM raw_source
